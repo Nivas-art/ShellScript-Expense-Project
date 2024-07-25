1451a0651d0d7e0c010816f3b757dd01546fc912
Nivas-art <srinujunnu123@gmail.com>
2024-07-25 19:11:58 +0530

    some

---
+++ db.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+TIMESTAMP=$(date -%F+%h+%m+%s)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+USERID=$(id -u)
+if [ $USERID -ge 0 ]
+then 
+    echo "your not in root user"
+    exit 1
+else
+    echo "your in root user"
+fi
+
+VALIDATE(){
+    if [ $1 -ge 0 ]
+then
+    echo "$2....FAILURE"
+    exit 1
+else
+    echo "$2....PASS"
+fi
+}
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "insatllation of mysql"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "enabale of mysqld"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "started"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "password is set"
