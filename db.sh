859a15cd09bc2fb1016d8fe9457f463f3b08ce8d
Nivas-art <srinujunnu123@gmail.com>
2024-07-25 20:56:09 +0530

    some

--- db.sh
+++ db.sh
@@ -12,10 +12,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
 then
-    echo "$2....FAILURE"
+    echo "$2....$R FAILURE $N"
     exit 1
 else
-    echo "$2....PASS"
+    echo "$2....$G PASS $N"
 fi
 }
 
