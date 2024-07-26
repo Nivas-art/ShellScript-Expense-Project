af6826a6c9396a91c82e7bb2be17257b35774b24
Nivas-art <srinujunnu123@gmail.com>
2024-07-26 22:02:26 +0530

    some

--- memorycheck.sh
+++ memorycheck.sh
@@ -9,6 +9,6 @@ USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
 FOLEDR=$(echo $line | awk -F " " '{print $NF}')
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then 
-    echo "$FOLEDR is more than $DISK_THRESHOLD: current usage: $USAGE"
+    echo "$FOLEDR: is more than $DISK_THRESHOLD: current usage: $USAGE"
 fi
 done <<<$DISK_USAGE
