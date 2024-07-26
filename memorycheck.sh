3fb1e42fabfc67a60e51a0c1a6faa2c75f3967b5
Nivas-art <srinujunnu123@gmail.com>
2024-07-26 22:06:47 +0530

    some

--- memorycheck.sh
+++ memorycheck.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
+MESSAGE=" "
 
 while IFS= read -r line
 do
@@ -9,6 +10,8 @@ USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
 FOLEDR=$(echo $line | awk -F " " '{print $NF}')
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then 
-    echo "$FOLEDR: is more than $DISK_THRESHOLD: current usage: $USAGE"
+    MESSAGE+="$FOLEDR: is more than $DISK_THRESHOLD: current usage: $USAGE"
 fi
 done <<<$DISK_USAGE
+
+echo -e message: $MESSAGE"
